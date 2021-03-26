3ac1919da51d104876cfa7e4c4e6ee63637377e3
Ivan Buccella <ivan@buccella.me>
2021-03-27 00:19:06 +0100

    Created Home and Projects pages.

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,8 +1,50 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 
 class AboutMe extends React.Component {
   render() {
-    return "About Me";
+    return <>
+      <div className="page about-me">
+        <div className="personal-information">
+          <div className="left">
+            <img src="/images/ivan-buccella.jpg" />
+            <div className="icons-container">
+              <a className="link" href="https://it.linkedin.com/in/ivanbuccella" target="_blank">
+                <FontAwesomeIcon
+                  icon={faLinkedinIn}
+                  size="2x"
+                />
+              </a>
+              <a className="link" href="mailto:ivan@buccella.dev?subject=Mail from Website">
+                <FontAwesomeIcon
+                  icon={faEnvelope}
+                  size="2x"
+                />
+              </a>
+            </div>
+          </div>
+          <div className="right">
+            <p className="role">Software Engineer</p>
+            <p className="name">Ivan Buccella</p>
+            <p className="description">
+              Hi! I'm a software engineer @<a target="_blank" href="https://beefree.io">BEE By MailUp Group</a>.<br />
+              I am a web developer with good knowledge of front-end techniques, but I love working on back-end environments too.<br />
+              I'm a fan of "pair programming" and I love working in a team, we'll learn faster and much more.<br />
+              Ah, yeah, I love the order...!<br /><br />
+              Let's connect on <a className="link" href="https://it.linkedin.com/in/ivanbuccella" target="_blank">Linkedin</a>&nbsp;
+              or <a className="link" href="mailto:ivan@buccella.dev?subject=Mail from Website">email</a>.
+            </p>
+            <div className="cta-container">
+              <a className="cta" href="/files/CV-Ivan-Buccella.pdf" target="_blank">
+                Download CV
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>;
   }
 }
 export default AboutMe;
--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -1,8 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
 class Projects extends React.Component {
   render() {
-    return "Projects";
+    return <>
+      <div className="page projects">
+        <h1 className="page-title">
+          Projects
+        </h1>
+        <div className="all-projects">
+          <div className="project">
+            <p className="title">
+              GigAdvisor
+            </p>
+            <ul>
+              <li>
+                Presentation (in Italian):&nbsp;
+                <Link to="https://docs.google.com/presentation/d/1AMMAF0Evw_Lc1XtkCiCrGCGbCxCXXWFpuszahMajb-A/">here</Link>
+              </li>
+              <li>
+                GitHub Repo:&nbsp;
+                <Link to="https://github.com/IvanBuccella/GigAdvisor">here</Link>
+              </li>
+              <li>
+                WebApp URL:&nbsp;
+                <Link to="https://gigadvisor.buccella.me/">here</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>;
   }
 }
 export default Projects;
