cb9e83f07285555abcaf1d7f3acafea7b616687f
Ivan Buccella <ivan@buccella.me>
2021-04-01 00:15:08 +0200

    Updated Projects page.

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -12,6 +12,13 @@ class Projects extends React.Component {
             <h2 className="title">
               GigAdvisor
             </h2>
+            <p>
+              GigAdvisor is an experimental platform designed to work within the scenario of gig economy, a phenomenon marked by a variety of issues - low levels of social protection for gig-workers; pervasive algorithmic control; atomization of work processes, etc. - that increase the need for more insightful empirical investigations and effective regulatory solutions.
+              <br />
+              The platform allows crowdworkers to express, share and discuss different kinds of assessments (by means of predefined scales and free text) on the quality of the experiences with the platforms they work for.
+              <br />
+              The tool stems from a research project that put critical data studies and data-driven computational social science to explore new approaches to the analysis and regulation of digital economy.
+            </p>
             <ul>
               <li>
                 Presentation (in Italian):&nbsp;
